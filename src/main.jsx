9544527a8f179f8c881469d40657017d2d2f5f4c
hamdyyemadd <hamdyyemad@aol.com>
2024-10-12 21:24:02 +0300

    reverting changes
    reverting changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import ReactDOM from "react-dom/client"; // Correct import
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-// Create a root using createRoot
-const root = ReactDOM.createRoot(rootElement);
-
-// Use hydrateRoot for SSR or normal rendering based on whether the root has children
-if (rootElement.hasChildNodes()) {
-  root.hydrate(<App />); // For SSR content
-} else {
-  root.render(<App />); // For client-side rendering
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
