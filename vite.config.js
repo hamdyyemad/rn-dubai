ea47af8d08bdd1e4828c006e6e127a1a2bdc1867
hamdyyemadd <hamdyyemad@aol.com>
2024-10-12 20:51:00 +0300

    adding react-snap to build/dist
    adding react-snap to build/dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build", // Change this to 'build' if you want to use the default
+    // ...other configurations
+  },
   plugins: [react(), eslint()],
 });
